bc740ec3944a555e7a22f411248d476e9ed4cb22
lucas <lucasvanpraag@hotmail.fr>
2023-12-13 18:28:19 +0100

    Co-authored-by: Romain Croughs <itsKorzie@users.noreply.github.com>

--- client/main.c
+++ client/main.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h> // added by me
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -8,45 +9,80 @@
 #include <string.h>
 #include "../commun/commun.h"
 
+#include <pthread.h>
+
+
+void handler_singint(int signum) {
+    if (signum == SIGINT) {
+        pthread_exit(0);
+    }
+}
+
+
 int createConnection(char* ipAddress) {
+    //struct connexion con;
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(5555);
+
     inet_pton(AF_INET, ipAddress, &server_addr.sin_addr);
-    connect(socket_fd, (struct sockaddr_in*) &server_addr, sizeof(server_addr));
+
+    if (connect(socket_fd, (struct sockaddr_in*) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Couldn't connect to the server");
+        close(socket_fd);
+        exit(1);
+    }
     return socket_fd;
 }
 
 void sendFile(int socket_fd, char* path_to_image) {
-    FILE* file = fopen(path_to_image, "rb");
-    char buffer[20001];
-    size_t bytes = fread(buffer, 1, 20001, file);
-    write(socket_fd, buffer, bytes);
-    fclose(file);
+    if (access(path_to_image, F_OK) == 0) {
+        //printf("Path : %s\n\n", path_to_image);
+        FILE *file = fopen(path_to_image, "rb");
+        char buffer[20001];
+        size_t bytes = fread(buffer, 1, 20001, file);
+        write(socket_fd, buffer, bytes);
+        fclose(file);
+    } else {
+        perror("This file does not exist");
+    }
 }
 
 void killCommunication(int socket_fd) {
     write(socket_fd, "\0", 1);
 }
 
-char* catchResponse(int socket_fd) {
+void* catchResponse(void* socket_fd) {
+
     char response[999];
-    read(socket_fd, response, 999);
-    printf("%s \n", response);
-    return response;
+    int server_fd = *(int*)socket_fd;
+    while(read(server_fd, response, 999) > 0) {
+        printf("%s\n", response);
+
+    }
 }
 
 int main(int argc, char* argv[]) {
+
     char serverIP[15] = "127.0.0.1";
+    if (argc == 2) {
+        strncpy(serverIP, argv[1], 15);
+    }
     int server_fd = createConnection(serverIP);
-    sendFile(server_fd, "./img/36.bmp");
-    catchResponse(server_fd);
-    sendFile(server_fd, "./img/89.bmp");
-    catchResponse(server_fd);
-    sendFile(server_fd, "./large.bmp");
-    catchResponse(server_fd);
+    char path[1000];
+    pthread_t thread;
+    pthread_create(&thread, NULL, catchResponse, (void*)(&server_fd));
+    while (fgets(path, sizeof(path), stdin) > 0) {
+
+        int length = strlen(path);
+        path[length-1] = '\0';
+
+        sendFile(server_fd, path);
+        memset(path, 0, sizeof(path));
+
+    }
     killCommunication(server_fd);
     close(server_fd);
     return 0;
--- serveur/main.c
+++ serveur/main.c
@@ -9,6 +9,7 @@
 #include <dirent.h>
 #include "../commun/commun.h"
 #include <limits.h>
+#include <stdlib.h>
 
 #include "imgdist.h"
 
@@ -22,7 +23,7 @@ struct serverParams {
 
 struct queryResults {
     char* filePath;
-    unsigned int distance;
+    int distance;
 };
 
 struct imgArgs {
@@ -122,7 +123,12 @@ struct serverParams createServer() {
  */
 void sendResults(int socket_fd, char mostSimilarImage[], int distance) {
     char response[999];
-    sprintf(response, "Most similar image found: '%s' with a distance of %d", mostSimilarImage, distance);
+    // printf("\nDistance = %d\n", distance);
+    if (distance == -1) {
+        sprintf(response, "No similar image found (no comparison could be performed successfully).");
+    } else {
+        sprintf(response, "Most similar image found: '%s' with a distance of %d", mostSimilarImage, distance);
+    }
     write(socket_fd, response, 999);
 }
 
@@ -205,58 +211,61 @@ struct queryResults compareImages(char buffer[], int bufsize) {
 
         // On hash l'image à comparer afin de ne pas répéter inutilement son traitement ;
         uint64_t baseImage_hash;
-        PHashRaw(buffer, bufsize, &baseImage_hash);
+        if (PHashRaw(buffer, bufsize, &baseImage_hash)) {
 
-        // Structure des paramètres à passer à threadCompareImage
-        struct CompareThreadParams threadParams1 = {.fileList = &fileListThread1, .baseHash = baseImage_hash};
-        struct CompareThreadParams threadParams2 = {.fileList = &fileListThread2, .baseHash = baseImage_hash};
+            // Structure des paramètres à passer à threadCompareImage
+            struct CompareThreadParams threadParams1 = {.fileList = &fileListThread1, .baseHash = baseImage_hash};
+            struct CompareThreadParams threadParams2 = {.fileList = &fileListThread2, .baseHash = baseImage_hash};
 
-        // Lancement des Threads 1 et 2;
-        if (pthread_create(&thread_1, NULL, threadCompareImages, &threadParams1) != 0) {
-            perror("Error when creating Thread 1. (phtread_create() error)");
-        }
-        if (pthread_create(&thread_2, NULL, threadCompareImages, &threadParams2) != 0) {
-            perror("Error when creating Thread 2. (phtread_create() error)");
-        }
+            // Lancement des Threads 1 et 2;
+            if (pthread_create(&thread_1, NULL, threadCompareImages, &threadParams1) != 0) {
+                perror("Error when creating Thread 1. (phtread_create() error)");
+            }
+            if (pthread_create(&thread_2, NULL, threadCompareImages, &threadParams2) != 0) {
+                perror("Error when creating Thread 2. (phtread_create() error)");
+            }
 
-        // On récupère les résultats des threads ;
+            // On récupère les résultats des threads ;
 
-        struct queryResults* subResult1, *subResult2;
-        
-        if (pthread_join(thread_1, (void**)&subResult1) != 0) {
-            perror("Error joining Thread 1. (pthread_join() error)");
-            exit(1);
-        }
-        if (pthread_join(thread_2, (void**)&subResult2) != 0) {
-            perror("Error joining Thread 2. (pthread_join() error)");
-            exit(1);
-        }
+            struct queryResults* subResult1, *subResult2;
 
-        // Finalement, on compare les résultats et on utilise les données de celui qui aura trouvé
-        // la plus petite distance entre l'image de base et une image donnée stockée dans .filePath
+            if (pthread_join(thread_1, (void**)&subResult1) != 0) {
+                perror("Error joining Thread 1. (pthread_join() error)");
+                exit(1);
+            }
+            if (pthread_join(thread_2, (void**)&subResult2) != 0) {
+                perror("Error joining Thread 2. (pthread_join() error)");
+                exit(1);
+            }
 
-        if (subResult1->distance <= subResult2->distance) {
-            res.distance = subResult1->distance;
-            res.filePath = subResult1->filePath;
-            free(subResult2->filePath); // free parce que ne sera plus utilisé apres
+            // Finalement, on compare les résultats et on utilise les données de celui qui aura trouvé
+            // la plus petite distance entre l'image de base et une image donnée stockée dans .filePath
+
+            if (subResult1->distance <= subResult2->distance) {
+                res.distance = subResult1->distance;
+                res.filePath = subResult1->filePath;
+                free(subResult2->filePath); // free parce que ne sera plus utilisé apres
+            } else {
+                res.distance = subResult2->distance;
+                res.filePath = subResult2->filePath;
+                free(subResult1->filePath);
+            }
+
+            // ... et on oublie pas de libérer l'espace alloué !
+
+            free(subResult1);
+            free(subResult2);
+
+            freeFileList(&fileListThread1);
+            freeFileList(&fileListThread2);
+
         } else {
-            res.distance = subResult2->distance;
-            res.filePath = subResult2->filePath;
-            free(subResult1->filePath);
+            res.distance = -1;  // En cas de chemin non valide ou d'erreur d'ouverture du fichier
         }
 
-        // ... et on oublie pas de libérer l'espace alloué !
-
-        free(subResult1);
-        free(subResult2);
-
-        freeFileList(&fileListThread1);
-        freeFileList(&fileListThread2);
-    
     } else {
         printf("No image to compare in %s dir.\n", directory);
     }
-
     freeFileList(&fileList);  // En tout dernier lieu, on libère l'espace alloué à la liste globale des fichiers ;
     return res;
 }
@@ -307,7 +316,7 @@ void handleConnection(int socket) {
     int communicationStatus = 1;
     while (communicationStatus) {
         char buffer[20001];
-        int imageSize = readImage(socket, &buffer);
+        int imageSize = readImage(socket, buffer);
         if (imageSize == -1) {
             char err[] = "The image you passed exceed 20Kb";
             write(socket, err, strlen(err));
